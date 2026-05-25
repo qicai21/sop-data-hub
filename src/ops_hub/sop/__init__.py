811b433b41125f6be70ace49d9fef3a7248c0eaf
Guo Dongbei <qicai21@gmail.com>
2026-05-25 09:44:42 +0800

    chore: add sop package placeholder

---
+++ src/ops_hub/sop/__init__.py
@@ -0,0 +1,5 @@
+"""SOP runtime planning package.
+
+This package is intentionally minimal in the planning branch. Concrete runtime
+implementation should be introduced by follow-up implementation orders.
+"""
